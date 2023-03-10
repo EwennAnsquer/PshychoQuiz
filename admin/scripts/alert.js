99f8d105b6b4f5dbad9126bc72588b0cf42220e1
_inymi_ <123171087+inymi173@users.noreply.github.com>
2023-03-10 15:46:10 +0100

    Delete admin directory

--- admin/scripts/alert.js
+++
@@ -1,8 +0,0 @@
-
-var data = document.getElementById("jsParams").getAttribute("data-error");
-
-if(data==1){
-    alert("❌ Veuillez finir toutes les Questions avant d'envoyer le formulaire.");
-}else if(data==2){
-    alert("❌ Veuillez remplir tous les champs avant d'envoyer le formulaire.");
-}
