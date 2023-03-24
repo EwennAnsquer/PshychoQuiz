eab76f8719b594f3a3c080c3004d3a03e021ae3d
EwennAnsquer <ewenn.ansq@gmail.com>
2023-03-24 16:52:51 +0100

    Commencement de la page question

--- admin/questions.php
+++ admin/questions.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="css/button.css">
+    <link rel="stylesheet" href="css/questions.css">
 </head>
 <body>
     <?php
@@ -36,27 +37,58 @@
             </div>
             <div class="d-flex justify-content-center align-items-center">
                 <table>
-                    <tbody class="d-flex flex-row">
-                        <tr class="d-flex flex-column">
-                            <?php
-                                $dataQuestion=getAllQuestions($connexion);
-                                foreach($dataQuestion as $value){
-                                    ?>
-                                    <td class="border border-dark"><?php echo($value[0]);?></td>
-                                    <?php
-                                }
-                            ?>
-                        </tr>
-                        <tr class="d-flex flex-column">
-                            <?php
-                                foreach($dataQuestion as $value){
-                                    ?>
-                                    <td class="border border-dark"><?php echo($value[1]);?></td>
-                                    <?php
-                                }
-                            ?>
-                        </tr>
-                    </tbody>
+                    <tr class="d-flex flex-row w-100">
+                        <td class="d-flex justify-content-center align-items-center border border-dark idQuestion">ID</td>
+                        <td class="d-flex justify-content-left align-items-center border border-dark libQuestion">Libelle</td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark enjeuQuestion">Enjeu</td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark ps-e pe-2 typeQuestion">Type de Question</td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark ps-e pe-2 text-center scoreQuestion">Score<br>Dev</td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark ps-e pe-2 text-center scoreQuestion">Score<br>Res</td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark">
+                            <button class="btn hidden" name="id" value="">Modifier</button>
+                        </td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark">
+                            <button class="btn hidden" name="id" value="">Supprimer</button>
+                        </td>
+                    </tr>
+                    <?php 
+                        $dataQuestion=getAllQuestions($connexion);
+                        foreach($dataQuestion as $value){
+                    ?>
+                    <tr class="d-flex flex-row w-100">
+                        <td class="d-flex justify-content-center align-items-center border border-dark idQuestion"><?php echo($value[0]);?></td>
+                        <td class="d-flex justify-content-left align-items-center border border-dark libQuestion"><?php echo($value[1]);?></td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark enjeuQuestion"><?php echo($value[2]);?></td>
+                        <?php 
+                            if($value[3]==1){
+                                $typeQuestion="Fermée";
+                                $score=selectScoreFerme($value[0],$connexion);
+                                $scoreRes=$score[0];
+                                $scoreDev=$score[1];
+                            }else{
+                                $typeQuestion="Échelle";
+                                $score=selectScoreEchelle($value[0],$connexion);
+                                $scoreRes=$score[0];
+                                $scoreDev=$score[1];
+                            }
+                        ?>
+                        <td class="d-flex justify-content-center align-items-center border border-dark ps-2 pe-2 typeQuestion"><?php echo($typeQuestion); ?></td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark ps-e pe-2 scoreQuestion"><?php echo($scoreDev); ?></td>
+                        <td class="d-flex justify-content-center align-items-center border border-dark ps-e pe-2 scoreQuestion"><?php echo($scoreRes); ?></td>
+                        <td class="border border-dark">
+                            <form action="back-end/ModifierQuestion.php" method="post">
+                                <button type="submit" class="btn" name="id" value="<?php echo($value[0]); ?>">Modifier</button>
+                            </form>
+                        </td>
+                        <td class="border border-dark">
+                            <form action="back-end/SupprimerQuestion.php" method="post">
+                                <button type="submit" class="btn" name="id" value="<?php echo($value[0]); ?>">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+                        }
+                    ?>
                 </table>
             </div>
         </section>
